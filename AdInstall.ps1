408c662289ee7a9a9ec2b805b28d25fbc3e2d3fa
Visp80 <visp80@gmail.com>
2026-02-10 21:07:06 +0200

    1

---
+++ AdInstall.ps1
@@ -0,0 +1,18 @@
+# Завантаження AnyDesk
+$url = "https://download.anydesk.com/AnyDesk.exe"
+$installer = "$env:TEMP\AnyDesk.exe"
+Invoke-WebRequest -Uri $url -OutFile $installer
+
+# Встановлення в кастомну папку
+$installPath = "C:\Program Files\AnyDesk"
+$password = "ВашПароль123"
+
+# Тиха інсталяція
+Start-Process -FilePath $installer -ArgumentList "--install `"$installPath`" --silent" -Wait
+
+# Налаштування пароля (після встановлення)
+$anydesk = "$installPath\AnyDesk.exe"
+Start-Process -FilePath $anydesk -ArgumentList "--set-password $password" -Wait
+
+# Видалення інсталятора
+Remove-Item $installer
