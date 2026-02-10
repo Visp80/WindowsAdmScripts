8c7a511e2d853867ae1ea68e3dcaa4d592903561
Visp80 <visp80@gmail.com>
2026-02-10 21:11:08 +0200

    1

--- AdInstall.ps1
+++ AdInstall.ps1
@@ -5,7 +5,7 @@ Invoke-WebRequest -Uri $url -OutFile $installer
 
 # Встановлення в кастомну папку
 $installPath = "C:\Program Files\AnyDesk"
-$password = "ВашПароль123"
+$password = "7777777"
 
 # Тиха інсталяція
 Start-Process -FilePath $installer -ArgumentList "--install `"$installPath`" --silent" -Wait
@@ -15,4 +15,4 @@ $anydesk = "$installPath\AnyDesk.exe"
 Start-Process -FilePath $anydesk -ArgumentList "--set-password $password" -Wait
 
 # Видалення інсталятора
-Remove-Item $installer
+#Remove-Item $installer
